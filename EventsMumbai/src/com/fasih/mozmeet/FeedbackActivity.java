c06b5d48b1e8dffa5c1babced6b40f41e2dcd4d7
Fasih Khatib <littlejavachild@yahoo.com>
2014-07-20 14:49:08 +0530

    Removed the ratings bar library and copied the files instead to the project

--- EventsMumbai/src/com/fasih/mozmeet/FeedbackActivity.java
+++ EventsMumbai/src/com/fasih/mozmeet/FeedbackActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class FeedbackActivity extends Activity {
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
